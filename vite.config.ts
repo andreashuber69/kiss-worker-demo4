10891ebb7a38958bbce064fc1e64991b7f9a6e78
Andreas Huber <andreashuber69@users.noreply.github.com>
2024-04-21 12:20:10 +0200

    docs: document reason for non-default build target

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     build: {
+        // The target setting only needs to be modified to support
+        // top-level await (which is used in index.html for simplicity).
+        // If you refrain from using top-level await, the default target
+        // would work just fine.
         target: "es2022",
         sourcemap: true,
     },
