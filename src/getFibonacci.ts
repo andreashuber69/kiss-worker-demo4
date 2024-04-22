51050fe1895925b1405d55aac98c90f97d1224e4
Andreas Huber <andreashuber69@users.noreply.github.com>
2024-04-22 12:58:55 +0200

    docs: clarify comment

--- src/getFibonacci.ts
+++ src/getFibonacci.ts
@@ -4,7 +4,8 @@ import { serve } from "kiss-worker";
 const getFibonacci = (n: number): number =>
     (n < 2) ? Math.floor(n) : getFibonacci(n - 1) + getFibonacci(n - 2);
 
-// Serve the function, so that it can be called from another thread
+// Serve the function, so that it can be called from the thread that
+// calls implementWorkerExternal
 serve(getFibonacci);
 
 // Export the type only
