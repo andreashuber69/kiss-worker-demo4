53b8935478cf54ed66595f57e9ff398295edfe6a
Andreas Huber <andreashuber69@users.noreply.github.com>
2024-05-05 10:28:08 +0200

    chore: adapt description

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
     build: {
         // The target setting only needs to be modified to support
-        // top-level await (which is used in index.html for simplicity).
+        // top-level await (which is used in main.ts for simplicity).
         // If you refrain from using top-level await, the default target
         // will work just fine.
         target: "es2022",
