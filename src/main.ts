c8dc616dc54a4835188fda91a813be9e937df052
Andreas Huber <andreashuber69@users.noreply.github.com>
2024-05-09 10:29:24 +0200

    fix: import

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { createCalculatorWorker } from "./createCalculatorWorker.ts";
+import { createCalculatorWorker } from "./createCalculatorWorker.js";
 
 // Start a new worker thread waiting for work.
 const worker = await createCalculatorWorker();
